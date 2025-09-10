654680958c47c249eb814fe678b31adaa46d2e7f
Asadbek <asadbeknan5566@gmail.com>
2025-09-10 16:38:46 +0500

    fix font

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
-
-import NunitoSansRegular from './assets/fonts/nunito-sans-v18-latin-regular.woff2'
-import NunitoSansSemiBold from './assets/fonts/nunito-sans-v18-latin-600.woff2'
-import NunitoSansBold from './assets/fonts/nunito-sans-v18-latin-700.woff2'
-
-const myCustomFont = {
-  fontFamily: 'Nunito Sans',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
-  fontWeight: 400,
-  src: `
-    local('MyCustomFont'),
-    local('MyCustomFont-Regular'),
-    url(${NunitoSansRegular}) format('woff2')
-  `,
-};
-
-const myCustomFontBold = {
-  fontFamily: 'Nunito Sans Bold',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
-  fontWeight: 700,
-  src: `
-    local('MyCustomFont Bold'),
-    local('MyCustomFont-Bold'),
-    url(${NunitoSansSemiBold}) format('woff2')
-  `,
-};
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import NunitoSansRegular from "./assets/fonts/nunito-sans-v18-latin-regular.woff2";
+import NunitoSansSemiBold from "./assets/fonts/nunito-sans-v18-latin-600.woff2";
+import NunitoSansBold from "./assets/fonts/nunito-sans-v18-latin-700.woff2";
 
 const theme = createTheme({
   typography: {
     fontFamily: [
-      'Nunito Sans',
-      'Roboto',
+      "Nunito Sans",
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-    ].join(','),
+      "Arial",
+      "sans-serif",
+    ].join(","),
   },
   components: {
     MuiCssBaseline: {
@@ -52,21 +26,28 @@ const theme = createTheme({
           font-style: normal;
           font-display: swap;
           font-weight: 400;
-          src: local('Nunito Sans'), local('MyCustomFont-Regular'), url(${myCustomFont}) format('woff2');
+          src: local('Nunito Sans'), url(${NunitoSansRegular}) format('woff2');
+        }
+        @font-face {
+          font-family: 'Nunito Sans';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 600;
+          src: local('Nunito Sans SemiBold'), url(${NunitoSansSemiBold}) format('woff2');
         }
         @font-face {
           font-family: 'Nunito Sans';
           font-style: normal;
           font-display: swap;
           font-weight: 700;
-          src: local('Nunito Sans Bold'), local('MyCustomFont-Bold'), url(${myCustomFontBold}) format('woff2');
+          src: local('Nunito Sans Bold'), url(${NunitoSansBold}) format('woff2');
         }
       `,
     },
   },
 });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -75,4 +56,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
-)
+);
